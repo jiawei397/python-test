94feb0f751499c77a804278720ed3594d962218c
jw397 <jw397@126.com>
2019-08-01 14:20:15 +0800

    feat: 请求

--- src/requests/test1.py
+++ src/requests/test1.py
@@ -2,5 +2,9 @@ import requests
 
 r = requests.get('https://www.baidu.com/')
 print(r.status_code)
-print(r.text)
+# print(r.text)
 
+r2 = requests.get('https://www.douban.com/', headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'})
+# print(r2.text)
+print(r2.headers['Content-Type'])
+# print(r2.cookies['ts'])
