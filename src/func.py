739169001923b47b367b360dce88ea3a53c15360
jiawei397 <jwqd397>
2019-08-11 19:44:06 +0800

    feat: mysql

--- src/func.py
+++ src/func.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from src.modules import *
+# from Test import test
+from jw import test
 
 def print_func(par):
   print("Hello : ", par)
-  return
+  # return
 
 
-def test(par):
+def test00000000000(par):
   if __name__ == '__main__':
     print('程序自身在运行')
   else:
@@ -19,3 +21,5 @@ def test(par):
 # test1.runnoob1()
 # test1.runnoob1()
 test2.runnoob2()
+
+test.a()
