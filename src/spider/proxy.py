7d6f6cec6408cd683f34898faa9842f793cb4987
jw397 <jw397@126.com>
2019-08-17 13:41:00 +0800

    feat: 设置代理

---
+++ src/spider/proxy.py
@@ -0,0 +1,36 @@
+import re
+import requests
+import urllib.request
+from lxml import etree
+
+url = 'https://www.qidian.com/rank/hotsales'
+# 浏览器伪装
+# opener=urllib.request.build_opener()
+
+# 设置代理
+proxy = {'http': '120.83.110.50:9999'}
+proxy_support = urllib.request.ProxyHandler(proxy)
+opener = urllib.request.build_opener(proxy_support)
+opener.addheaders = [('User-Agent',
+                      'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')]
+urllib.request.install_opener(opener)
+html = urllib.request.urlopen(url).read().decode('utf-8')
+
+# html=requests.get(url).text
+# f = open('FC.html','w')
+# f.write(str(html))
+# f.close()
+# print(html)
+
+selector = etree.HTML(html)
+info = selector.xpath('//div[@class="book-mid-info"]')
+
+for each in info:
+    name = each.xpath('h4/a[@data-eid="qd_C40"]/text()')
+    author = each.xpath('p[@class="author"]/a[@class="name"]/text()')
+    target = each.xpath('p[@class="author"]/a[@data-eid="qd_C42"]/text()')
+    intro = each.xpath('p[@class="intro"]/text()')[0]
+    print(name)
+    print(author)
+    print(target)
+    print(intro + '\n')
