081f463cb3e366d8799ebefcfd2327f91e0eba10
jiawei397 <jwqd397>
2019-11-14 22:39:59 +0800

    feat: 处理清单

--- src/excel/write.py
+++ src/excel/write.py
@@ -3,52 +3,20 @@ import os
 import re
 
 file = 'demo.xlsx'
+qingdan_dict = {}
+end_row_map = {}
+pattern = re.compile(r'^[-+]?[-0-9]\d*\.\d*|[-+]?\.?[0-9]\d*$')
+map = {}
 
 # 遍历指定目录，显示目录下的所有文件名
 def eachFile(filepath):
     pathDir = os.listdir(filepath)
+    num = 1
     for allDir in pathDir:
         child = os.path.join('%s/%s' % (filepath, allDir))
         # print(child)  # .decode('gbk')是解决中文显示乱码问题
-        read_excel(child)
-
-    # print(list)
-    # print(sum(list))
-
-
-
-
-# 读取文件内容并打印
-# def readFile(filename):
-#     fopen = open(filename, 'r')  # r 代表read
-#     for eachLine in fopen:
-#         print("读取到得内容如下：", eachLine)
-
-
-
-# file = '集团财务报表编制表.xlsx'
-
-# list = []
-
-
-# def is_number(s):
-#     try:
-#         float(s)
-#         return True
-#     except ValueError:
-#         pass
-#
-#     try:
-#         import unicodedata
-#         unicodedata.numeric(s)
-#         return True
-#     except (TypeError, ValueError):
-#         pass
-#
-#     return False
-
-pattern = re.compile(r'^[-+]?[-0-9]\d*\.\d*|[-+]?\.?[0-9]\d*$')
-map = {}
+        read_excel(child, num)
+        num += 1
 
 def is_number(num):
   result = pattern.match(str(num))
@@ -64,7 +32,7 @@ def is_key(table, row, num):
     return get_key(table,row, num) in map
 
 
-def read_excel(file):
+def read_excel(file, num):
     data = openpyxl.load_workbook(file)
     # print(data.get_named_ranges())  # 输出工作页索引范围
     # print(data.get_sheet_names())  # 输出所有工作页的名称
@@ -77,6 +45,8 @@ def read_excel(file):
     for table in data.worksheets:
         if table.title.find('清单') == -1 and table.title.find('填写须知') == -1:
             read_table(table)
+        elif table.title.find('清单') != -1:
+            read_qingdan_table(table, num)
 
     # table = data.worksheets[2]
 
@@ -95,25 +65,18 @@ def read_table(table):
     # print(table.title)  # 输出表名
     nrows = table.max_row  # 获得行数
     ncolumns = table.max_column  # 获得行数
-    # print(ncolumns)
 
     for row in range(1, nrows):
         for col in range(1, ncolumns):
             val = table.cell(row, col).value
             if val and is_number(val):
-                # print('%s/%s/%s' % (row, col, val))
                 key = get_key(table.title, row, col)
                 map.setdefault(key, [])
                 map[key].append(val)
 
-# read_excel(file)
-# print(map)
 
 def write_excel(file):
     data = openpyxl.load_workbook(file)
-    # table = data.worksheets[2]
-    # table.cell(4, 4).value = val
-    # data.save('excel_test.xlsx')
     for table in data.worksheets:
         if table.title.find('清单') == -1 and table.title.find('填写须知') == -1:
             nrows = table.max_row  # 获得行数
@@ -124,7 +87,53 @@ def write_excel(file):
                     if is_key(table.title, row, col):
                         table.cell(row, col).value = sum(map[get_key(table.title, row, col)])
 
+        #TODO elif table.title.find('清单') != -1:
+        elif table.title == '4.应收清单20190930':
+        #     print(table.title)
+            for key in qingdan_dict:
+                arr = key.split('-')
+                if arr[0] == table.title:
+                    print(key, qingdan_dict[key])
+                    table.cell(int(arr[1]), int(arr[2])).value = qingdan_dict[key]
+
     data.save('汇总表.xlsx')
+
+# 读取清单
+def read_qingdan_table(table, num):
+    nrows = table.max_row  # 获得行数
+    ncolumns = table.max_column  # 获得行数
+    # print(nrows)
+
+    end_row = nrows
+
+    if table.title not in end_row_map:
+        for row in range(3, nrows):
+            for col in range(1, ncolumns):
+                val = table.cell(row, col).value
+                if val != None:
+                    if val == '合计':
+                        # print(row, col)
+                        end_row = row - 1
+                        end_row_map[table.title] = end_row
+    else:
+        end_row = end_row_map[table.title]
+
+    for row in range(3, end_row):
+        for col in range(1, ncolumns):
+            val = table.cell(row, col).value
+            if val != None:
+                # print(row, col, val)
+                key = get_key(table.title, row - 2 + (num-1)* end_row, col)
+                qingdan_dict[key] = val
+
+
+# def read_qingdan(file):
+#     data = openpyxl.load_workbook(file)
+#     table = data.get_sheet_by_name('4.应收清单20190930')  # sheetnames[2]
+#     read_qingdan_table(table)
+#     print(qingdan_dict)
+
+# read_qingdan(file)
 
 eachFile('data')
 write_excel(file)
